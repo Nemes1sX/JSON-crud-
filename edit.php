cbd4007fde06ec53b202090f68a034d23813625b
Nemes1sX <kazstonk@gmail.com>
2021-12-29 23:38:32 +0200

    Forms are made

--- edit.php
+++ edit.php
@@ -1,1 +1,30 @@
 <?php
+
+include 'partials/header.php';
+require __DIR__ . '/contacts.php';
+
+if (!isset($_GET['id'])) {
+    include "partials/404.php";
+    exit;
+}
+$contactId = $_GET['id'];
+
+$contact = getcontactById($contactId);
+if (!$contact) {
+    include "partials/404.php";
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $contact = array_merge($contact, $_POST);
+
+    $contact = updateContact($_POST, $contactId);
+    header("Location: index.php");
+    }
+
+
+
+
+?>
+
+<?php include '_form.php' ?>
